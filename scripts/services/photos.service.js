2517e7bedfa02e34612ce1044aa6c0e463e257fd
Mikolaj Wesolowski <westa@poczta.onet.pl>
2018-11-29 15:15:58 +0100

    czary mary po obiedzie

--- scripts/services/photos.service.js
+++ scripts/services/photos.service.js
@@ -2,12 +2,20 @@
 
     class PhotosService {
         fetchPhotos() {
-           return window.fetch('backend/db.json')
+            return window.fetch('backend/db.json')
                 .then((response) => {
                     return response.json();
                 })
         }
+
+        async fetchPhotosAsync() {
+            const response = await window.fetch('backend/db.json');
+            const data = await response.json();
+            return data;
+        }
     }
+
     window.gallery = window.gallery || {};
     window.gallery.PhotosService = new PhotosService();
+
 })();
